48bd8ccc311108e661454bf476f8a985b0822ee5
Joao Oliveira <up202007580@up.pt>
2024-03-24 19:41:40 +0000

    Read db.txt and parse type/quantity

--- MES/src/main.cpp
+++ MES/src/main.cpp
@@ -1,7 +1,31 @@
 #include <iostream>
+#include <fstream>
+#include <vector>
+#include <string>
+#include <sstream>
+
+void read_txt(const std::string &filename, std::vector<int> &type, std::vector<int> &quantity)
+{
+    std::ifstream file(filename);
+    std::string line;
+    while (std::getline(file, line))
+    {
+        std::istringstream iss(line);
+        int column1, column2;
+        if (iss >> column1 >> column2)
+        {
+            type.push_back(column1);
+            quantity.push_back(column2);
+        }
+    }
+    file.close();
+}
 
 int main()
 {
-    std::cout << "Hello, World!" << std::endl;
+    std::vector<int> type;
+    std::vector<int> quantity;
+    read_txt("MES/db.txt", type, quantity);
+
     return 0;
 }
