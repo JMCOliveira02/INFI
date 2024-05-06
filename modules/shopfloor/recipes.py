f67ea5bfbaeb6eda1604ef10efde80c56ed2a9f4
Daniele da Mota Caldana <danielecaldana02@gmail.com>
2024-05-06 09:32:35 +0100

    refactor

--- modules/shopfloor/recipes.py
+++ modules/shopfloor/recipes.py
@@ -1,30 +1,23 @@
-import modules.communications.plc_communications as PLCCommunications
-
-
 '''
 Class responsável por representar uma receita de produção. Assim como
 enviá-las para o PLC
 '''
 class Recipe():
-    def __init__(self, client: PLCCommunications):
-        self.client = client
-        self.recipe_id = None
-        self.machine_id = None
-        self.piece_in = None
-        self.tool = None
-        self.time = None
-
-    
-
-    def sendRecipe(self):
-        '''
-        Função que envia a receita para o PLC
-
-        args:
-            None
-        return:
-            None
-        '''
-        self.client.sendRecipe(self)
-
-    
+    def __init__(self, order_id: int, global_id: int, target_piece: int):
+        self.order_id = order_id
+        self.global_id = global_id # id único da receita
+        self.recipe_id = None # o manager atribui um id único a cada receita. Se None, significa que a receita ainda não foi lançada para o shopfloor nem foi considerada para escalonamento
+        self.machine_id = None # preenchido pelo escalonamento
+        self.piece_in = None # preeenchido pelo escalonamento
+        self.piece_out = None # preenchido pelo escalonamento
+        self.target_piece = target_piece # peça a produzir
+        self.tool = None # preenchido pelo escalonamento
+        self.time = None # preenchido pelo escalonamento
+        self.end = False # indica se a receita foi terminada pelo PLC
+        self.current_transformation = None # indica a transformação atual. Por exemplo, para uma target_piece=3, a primeira transformação é 1->2, a segunda 2->3. Preenchido pelo escalonamento
+        #self.in_production = False # indica se a receita está em produção, ou seja, que foi lançada para o shopfloor
+        self.sended_date = None
+        self.finished_date = None
+        self.current_date = None
+        self.tolerance = 0 # tolerância após verificar estado da receita no PLC tendo passado o tempo de transformação. 
+                           # Se a receita não estiver terminada, é necessário esperar mais tempo, ou seja, tolerência aumenta
