55fbdb74dba6068471cdc6bd7064818ef4e47094
Daniele da Mota Caldana <danielecaldana02@gmail.com>
2024-05-06 17:10:03 +0100

    added comment

--- modules/mes/production_order.py
+++ modules/mes/production_order.py
@@ -3,6 +3,12 @@
 
 class ProductionOrder():
     def __init__(self, order_id: int, production_order: list[tuple[int, int, str]]):
+        '''
+        Construtor da classe ProductionOrder
+        Args:
+            order_id (int): id da ordem de produção
+            production_order (list[tuple[int, int, str]]): lista com a ordem de produção. Cada elemento é uma tupla com a peça a produzir, a quantidade e a data de início
+        '''
         # atributos da ordem de produção
         self.order_id = order_id
         self.target_piece = production_order[0]
