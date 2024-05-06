775a13be62d33f654a42e8e443ebee908b308ab6
Daniele da Mota Caldana <danielecaldana02@gmail.com>
2024-05-06 09:31:14 +0100

    minor correction

--- utils.py
+++ utils.py
@@ -22,15 +22,17 @@ class bcolors:
 
 
 
-def printAuthorsCredits():
-    print("\n***************************************************")
-    print(emoji.emojize("Academic Year: 2023/2024 :calendar:"))
-    print(emoji.emojize("Course: Informática Indústrial (INFI) :TOP_arrow:"))
-    print("Students:")
-    print(emoji.emojize(":right_arrow:  Carlos Thadeu Aguiar de Faria - up202202472"))
-    print(emoji.emojize(":right_arrow:  Daniele da Mota Caldana - up202308625"))
-    print(emoji.emojize(":right_arrow:  João Miguel de Carvalho Oliveira - up202007580"))
-    print(emoji.emojize(":right_arrow:  Joana Sofia Pais Silva - up202006241"))
-    print(emoji.emojize(":right_arrow:  Thiago Baldassarri Levin - up201900223"))
-    print("***************************************************\n")
-    print(emoji.emojize(f':rocket:  {bcolors.HEADER}Starting MES{bcolors.ENDC} :rocket:\n'))
+def printAuthorsCredits(show_credits=True):
+    print(chr(27) + "[2J")
+    if show_credits:
+        print("\n***************************************************")
+        print(emoji.emojize("Academic Year: 2023/2024 :calendar:"))
+        print(emoji.emojize("Course: Informática Indústrial (INFI) :TOP_arrow:"))
+        print("Students:")
+        print(emoji.emojize(":right_arrow:  Carlos Thadeu Aguiar de Faria - up202202472"))
+        print(emoji.emojize(":right_arrow:  Daniele da Mota Caldana - up202308625"))
+        print(emoji.emojize(":right_arrow:  João Miguel de Carvalho Oliveira - up202007580"))
+        print(emoji.emojize(":right_arrow:  Joana Sofia Pais Silva - up202006241"))
+        print(emoji.emojize(":right_arrow:  Thiago Baldassarri Levin - up201900223"))
+        print("***************************************************\n")
+        print(emoji.emojize(f':rocket:  {bcolors.HEADER}Starting MES{bcolors.ENDC} :rocket:\n'))
