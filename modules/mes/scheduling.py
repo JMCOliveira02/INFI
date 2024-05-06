2569de3769c83cd0c40d23fac309e83bb711a8dc
Daniele da Mota Caldana <danielecaldana02@gmail.com>
2024-05-06 17:08:33 +0100

    refactor

--- modules/mes/scheduling.py
+++ modules/mes/scheduling.py
@@ -3,6 +3,7 @@ import datetime
 from modules.communications.plc_communications import PLCCommunications
 from modules.mes.transformations import *
 from modules.shopfloor.recipes import Recipe
+from utils import date_diff_in_Seconds
 
 
 
@@ -106,14 +107,15 @@ class Scheduling():
 
 
 
-    def __calculateEdgesWeights(self, edges):
+    def __calculateEdgesWeights(self, transform: list, is_even: bool = True):
         '''
         Função para calcular o peso de todas as arestas com base no tempo de transformação, 
         tempo de mudança de ferramenta (se necessário), tempo de manutenção (se necessário),
         se a respetiva máquina está a operar e prioridade de transformação das máquinas de id par.
         Args:
-            edges: lista de arestas obtidas das transformações simples. Deste modo evita-se calcular
+            transform: lista dos dois nós obtidos das transformações simples. Deste modo evita-se calcular
             pesos para arestas que não têm interesse para esta transformação.
+            is_even: se True, a prioridade de transformação é para as máquinas de índice par.
 
         Return:
             -1: Todas as máquinas estão ocupadas. É melhor esperar.
@@ -124,8 +126,17 @@ class Scheduling():
         # atualizar ferramenta das máquinas
         updateMachineTool(self.client)
         busy_machines = 0
+        n_edges = 0
         # cálculo do peso para cada edge
         for edge in self.G.edges(data=True, keys=True):
+            if edge[0] != transform[0] or edge[1] != transform[1]:
+                continue
+            # atualizar número de edges válidos para esta transformação
+            if is_even and edge[3]['machine_id'] % 2 == 0: # prioridade para as máquinas de índice par
+                n_edges += 1 # para saber quantas arestas existem e comparar com o número de máquinas ocupadas
+            elif not is_even and edge[3]['machine_id'] % 2 != 0: # prioridade para as máquinas de índice ímpar
+                n_edges += 1
+
             edge_time, tool_time, main_time, machine_time, previous_machine_time = 0, 0, 0, 0, 0
             # tempo de transformação
             edge_time = edge[3]['time']
@@ -142,12 +153,12 @@ class Scheduling():
             # tempo de máquina atual (verificar se máquina está a operar)
             if cur_machine_state[edge[3]['machine_id']] == 1:
                 machine_time = Tmachine
-                busy_machines += 1
+                if is_even and edge[3]['machine_id'] % 2 == 0: # prioridade para as máquinas de índice par
+                    busy_machines += 1
+                elif not is_even and edge[3]['machine_id'] % 2 != 0:
+                    busy_machines += 1
         
             # tempo de máquina anterior (verificar se máquina anterior, de índice impar está ocupada)
-            '''
-            !!!!!!!!!PODE SER NECESSÁRIO OLHAR PARA AS RECEITAS E NÃO PARA O ESTADO ATUAL DA MÁQUINA
-            '''
             if edge[3]['machine_id'] % 2 == 0: # máquina de índice par
                 if cur_machine_state[edge[3]['machine_id'] - 1] == 0: # máquina anterior (índice ímpar) não está a produzir
                     previous_machine_time = Tprevious_free
@@ -158,7 +169,7 @@ class Scheduling():
             edge_weight = edge_time + tool_time + main_time + machine_time + previous_machine_time
             self.G[edge[0]][edge[1]][str(edge[0])+str(edge[1])+"M"+str(edge[3]["machine_id"])]['weight'] = edge_weight
         
-        if busy_machines == len(edges):
+        if busy_machines == n_edges:
             # indica que todas as máquinas estão ocupadas. É melhor esperar
             return -1
         return 0
@@ -222,6 +233,7 @@ class Scheduling():
         if status in {"waiting", "stashed"}:
             nodes, edges = findSimpleTransformations(self.G_simple, recipe.target_piece if status == "waiting" else recipe.piece_out)
             recipe.sended_date = datetime.datetime.now() if status == "waiting" else recipe.sended_date
+            recipe.finished_date = datetime.datetime.now() if status == "waiting" else recipe.finished_date
             if len(nodes) == 0 or len(edges) == 0:
                 return -2
             if status == "waiting":
@@ -229,36 +241,39 @@ class Scheduling():
                 if transform == -1:
                     return -3
         elif status == "active":
-            nodes, edges = findSimpleTransformations(self.G_simple, recipe.piece_out)
+            nodes, edges = findSimpleTransformations(self.G_simple, recipe.target_piece, recipe.piece_out)
+            transform = [nodes[0][0], nodes[0][1]]
             if len(nodes) == 0 or len(edges) == 0:
                 return -2
-
-        '''
-        CALCULAR COM BASE NO is_even !!!!!!!
-        '''
         
         # é possível realizar a transformação. Atualiza pesos das arestas.
-        all_machines_busy = self.__calculateEdgesWeights(edges)
+        all_machines_busy = self.__calculateEdgesWeights(transform, is_even)
 
         if all_machines_busy == -1:
             return all_machines_busy
 
         # escolher o caminho mais curto
         shortest_path = nx.shortest_path(self.G, source=transform[0], target=transform[1], weight='weight', method='dijkstra')
-        
         # obter todas as arestas do grafo
         all_edges = sorted(self.G.edges(data=True, keys=True), key=lambda x: x[3]['weight'])
 
-        # Filtrar as arestas que estão no caminho mais curto
+        # Filtrar as arestas que estão no caminho mais curto e que possuem os nós da transformação
         edges = [(u, v, k, d) for u, v, k, d in all_edges if (u, v) in zip(shortest_path, shortest_path[1:])]
+
+        # filtrar de acordo com a prioridade de transformação
+        if is_even:
+            edges = [edge for edge in edges if edge[3]['machine_id'] % 2 == 0]
+        else:
+            edges = [edge for edge in edges if edge[3]['machine_id'] % 2 != 0]
 
         recipe.machine_id = edges[0][3]['machine_id']
         recipe.piece_in = edges[0][0]
         recipe.piece_out = edges[0][1]
         recipe.tool = edges[0][3]['tool']
         recipe.time = edges[0][3]['time']
+        recipe.end = False
         recipe.current_transformation = (edges[0][0], edges[0][1])
-        recipe.finished_date = recipe.sended_date + datetime.timedelta(seconds=recipe.time/1000)
+        recipe.finished_date = recipe.sended_date + datetime.timedelta(seconds=date_diff_in_Seconds(recipe.finished_date, recipe.sended_date)) + datetime.timedelta(seconds=edges[0][3]["weight"]/1000)
         # recipe.in_production = True
 
         return recipe
