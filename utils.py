d868c359b30d5adcf17ba9229bb80af3ad4bf242
Daniele da Mota Caldana <danielecaldana02@gmail.com>
2024-05-07 14:01:28 +0100

    added codesys version

--- utils.py
+++ utils.py
@@ -29,14 +29,16 @@ def printAuthorsCredits(show_credits=True):
         print("\n***************************************************")
         print(emoji.emojize("Academic Year: 2023/2024 :calendar:"))
         print(emoji.emojize("Course: Informática Indústrial (INFI) :TOP_arrow:"))
-        print("Students:")
+        print("Software developed by:")
         print(emoji.emojize(":right_arrow:  Carlos Thadeu Aguiar de Faria - up202202472"))
         print(emoji.emojize(":right_arrow:  Daniele da Mota Caldana - up202308625"))
         print(emoji.emojize(":right_arrow:  João Miguel de Carvalho Oliveira - up202007580"))
         print(emoji.emojize(":right_arrow:  Joana Sofia Pais Silva - up202006241"))
         print(emoji.emojize(":right_arrow:  Thiago Baldassarri Levin - up201900223"))
+        print(emoji.emojize(":copyright: 2024 A02_B Group"))
         print("***************************************************\n")
         print(emoji.emojize(f':rocket:  {bcolors.HEADER}Starting MES{bcolors.ENDC} :rocket:\n'))
+        print(emoji.emojize(f':warning:  {bcolors.WARNING}Make sure you are running Codesys {bcolors.UNDERLINE}{CONSTANTS["codesys_version"]}{bcolors.ENDC}{bcolors.WARNING} program {bcolors.ENDC} :warning:'))
 
 
 
