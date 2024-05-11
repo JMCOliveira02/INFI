112c00210dc8e27704aea6ca4eacd049f31c89df
Daniele da Mota Caldana <danielecaldana02@gmail.com>
2024-05-11 19:38:12 +0100

    added clock class

--- mes/clock.py
+++ mes/clock.py
@@ -10,7 +10,7 @@ class Clock:
         self.db = Database()
         self.initil_date = self.db.update_initial_time()
         self.curr_time_seconds = date_diff_in_Seconds(datetime.datetime.now(), self.initil_date)
-        self.curr_day = self.curr_time_seconds / 60
+        self.curr_day = self.curr_time_seconds // 60
 
 
 
@@ -25,6 +25,7 @@ class Clock:
         '''
         self.curr_time_seconds = date_diff_in_Seconds(datetime.datetime.now(), self.initil_date)
         self.curr_day = self.curr_time_seconds // 60
+        self.curr_time_seconds = self.curr_time_seconds % 60
         return
     
 
@@ -35,6 +36,79 @@ class Clock:
         args:
             None
         return:
-            int, int: data atual em dias e segundos
+            tuple (int, int): data atual em dias e segundos
         '''
         return self.curr_day, self.curr_time_seconds
+    
+
+
+    def get_time_pretty(self):
+        '''
+        Função que retorna a data atual de forma legível
+
+        args:
+            None
+        return:
+            str: data atual em formato legível
+        '''
+        self.update_time()
+        return f'{self.curr_day}:{(self.curr_time_seconds * 24)//60} (D:H)'
+    
+
+
+    def get_time_seconds(self):
+        '''
+        Função que retorna a data atual em segundos
+
+        args:
+            None
+        return:
+            int: data atual em segundos
+        '''
+        return self.curr_time_seconds + self.curr_day * 60
+    
+
+
+    def diff_time(self, time: tuple[int, int]):
+        '''
+        Função que retorna a diferença entre a data atual e a data passada
+
+        args:
+            time (tuple[int, int]): data passada (dias, segundos)
+        return:
+            int: diferença entre as datas em segundos
+        '''
+        self.update_time()
+        time = time[0] * 60 + time[1]
+        return self.curr_time_seconds + self.curr_day * 60 - time
+    
+
+
+    def diff_between_times(self, time1: tuple[int, int], time2: tuple[int, int]):
+        '''
+        Função que retorna a diferença entre duas datas
+
+        args:
+            time1 (tuple[int, int]): primeira data (dias, segundos)
+            time2 (tuple[int, int]): segunda data (dias, segundos)
+        return:
+            int: diferença entre as datas em segundos
+        '''
+        time1 = time1[0] * 60 + time1[1]
+        time2 = time2[0] * 60 + time2[1]
+        return time1 - time2
+
+
+    
+    def add_seconds(self, curr_date: tuple[int, int], seconds: int):
+        '''
+        Função que adiciona segundos a data atual
+
+        args:
+            curr_date (tupple): data a somar os segundos
+            seconds (int): segundos a serem adicionados
+        return:
+            None
+        '''
+        new_date = curr_date[0] * 60 + curr_date[1] + seconds
+        return new_date // 60, new_date % 60
